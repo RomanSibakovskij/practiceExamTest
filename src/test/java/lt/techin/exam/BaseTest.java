64ab2c883ac206c13cf6283557c959815fa7d0c9
RomanSibakovskij <j4023958@gmail.com>
2024-08-22 04:23:02 +0300

    Setup BasePage/BaseTest

---
+++ src/test/java/lt/techin/exam/BaseTest.java
@@ -0,0 +1,33 @@
+package lt.techin.exam;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-search-engine-choice-screen");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("");
+    }
+
+
+
+    @AfterEach
+    public void close() {
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+
+}
